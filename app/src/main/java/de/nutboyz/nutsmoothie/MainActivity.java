6ea4a8ca2a8e0a23ff14d5e957c909d007959106
JanWe92 <Janweisenstein9270@gmail.com>
2016-03-30 14:13:14 +0200

    Added GoogleMap API Activity: - Show current location - Implemented handling of location change - Set marker for long press on the map
    You need a special key for the access of the googleAPI. I can show you how to get that key.

--- app/src/main/java/de/nutboyz/nutsmoothie/MainActivity.java
+++ app/src/main/java/de/nutboyz/nutsmoothie/MainActivity.java
@@ -1,15 +1,24 @@
 package de.nutboyz.nutsmoothie;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button get_map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        get_map = (Button) findViewById(R.id.btn_main_getMap);
+        get_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Intent intent = new Intent(MainActivity.this,Google_Map.class);
+                 startActivity(intent);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {

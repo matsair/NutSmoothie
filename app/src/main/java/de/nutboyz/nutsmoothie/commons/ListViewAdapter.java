c63bbb33419bf2f409bce1d3903b5d3836418e54
Johannes <Jojo@mobile-201-135.wlan.uni-mannheim.de>
2016-04-26 11:04:45 +0200

    initial commit

---
+++ app/src/main/java/de/nutboyz/nutsmoothie/commons/ListViewAdapter.java
@@ -0,0 +1,86 @@
+package de.nutboyz.nutsmoothie.commons;
+
+/**
+ * Created by Jojo on 21/04/16.
+ */
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+        import android.app.Activity;
+        import android.content.Context;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.CheckBox;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+
+        import de.nutboyz.nutsmoothie.R;
+
+
+public class ListViewAdapter extends ArrayAdapter<Task>{
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView name;
+        TextView author;
+        TextView duration;
+        CheckBox cBox;
+    }
+
+    Context context;
+    View convertView;
+    ArrayList<Task> data = new ArrayList<Task>();
+
+
+    public ListViewAdapter(Context context, ArrayList<Task> data) {
+
+        super(context, R.layout.activity_main, data);
+
+        this.context = context;
+        this.data=(ArrayList<Task>) data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        this.convertView = convertView;
+        Task bookStatus = getItem(position);
+
+
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // 2. Get rowView from inflater
+
+        View rowView =  inflater.inflate(R.layout.list_row, parent, false);
+
+            // 3. Get icon,title & counter views from the rowView
+            CheckBox checkView = (CheckBox) rowView.findViewById(R.id.main_list_checkbox);
+            TextView titleView = (TextView) rowView.findViewById(R.id.txtview_task_name);
+            TextView authorView = (TextView) rowView.findViewById(R.id.txtview_task_author);
+            TextView durationView = (TextView) rowView.findViewById(R.id.txtview_distance);
+
+
+            // 4. Set the text for textView
+//	                imgView.setImageResource(data.get(position));
+            titleView.setText(data.get(position).getName());
+           // authorView.setText(data.get(position).getAuthor());
+            durationView.setText("3");
+
+
+        // 5. retrn rowView
+        return rowView;
+
+    }
+
+
+    static class RecordHolder {
+        TextView txtTitle;
+        ImageView imageItem;
+
+    }
+
+}
